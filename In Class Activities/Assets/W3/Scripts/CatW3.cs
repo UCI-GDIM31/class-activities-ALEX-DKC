c5c893a5663c0d347ded80508f51b23ea8f5ca12
ALEX-DKC <dinga8@uci.edu>
2025-10-13 19:26:58 -0700

    new

--- In Class Activities/Assets/W3/Scripts/CatW3.cs
+++ In Class Activities/Assets/W3/Scripts/CatW3.cs
@@ -13,7 +13,7 @@ public class CatW3 : MonoBehaviour
     [SerializeField] private TMP_Text _healthText;
     [SerializeField] private TMP_Text _pointsText;
     [SerializeField] private TMP_Text _speechText;
-    [SerializeField] private float _maxHealth = 5;
+    [SerializeField] private float _maxHealth = 10;
     [SerializeField] private bool _destroyCatWhenDead;
 
     private bool _facingLeft;
@@ -95,7 +95,7 @@ public class CatW3 : MonoBehaviour
             // Below this comment, CALL the method named DecreaseHealth.
             // Notice this method's return type is void- that means we don't
             //      have to store the result anywhere.
-            
+            DecreaseHealth();
 
             // STEP 2 ---------------------------------------------------------
 
@@ -107,8 +107,11 @@ public class CatW3 : MonoBehaviour
             //
             // Try toggling the Destroy Cat When Dead setting on the Inspector,
             //      and see how the cat is removed ONLY when it's checked!
-            
 
+            if (_health <= 0 && _destroyCatWhenDead)
+            {
+                DestroyCat();
+            }
             // STEP 6 ---------------------------------------------------------
         }
     }
@@ -129,14 +132,14 @@ public class CatW3 : MonoBehaviour
     private void DecreaseHealth()
     {
         // write Step 3 below this comment!
-
-
+        _health--;
+        _healthText.text = "health = " + _health;
         // STEP 5 -------------------------------------------------------------
         // Once you've finished Step 4, CALL the GetHealthSpeechText method
         //      and store the result in _speechText's text variable.
         // This will look very similar to the above line to change _healthText ;)
 
-
+        _speechText.text = GetHealthSpeechText();
         // STEP 5 -------------------------------------------------------------
     }
     // STEP 3 -----------------------------------------------------------------
@@ -153,11 +156,17 @@ public class CatW3 : MonoBehaviour
     //      return "OH NO!".
     // 2. Otherwise, return "ouch".
 
-    //private ??? GetHealthSpeechText()
-    //{
-        // put the method body here!
-        
-    //}
+    private string GetHealthSpeechText()
+    {if (_health < _maxHealth / 2)
+        {
+        return "OH NO!";
+        }
+    
+        else
+        {
+        return "ouch";
+        }
+    }
     
     // STEP 4 -----------------------------------------------------------------
 
@@ -169,7 +178,7 @@ public class CatW3 : MonoBehaviour
         //      of the ball's ballRenderer's color variable.
         // This means you'll need to use the '.' twice to get to the color :)
 
-
+        _spriteRenderer.color = ball.ballRenderer.color;
         // STEP 7 -------------------------------------------------------------
     }
     
