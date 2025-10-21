2db74df657a526ff035d8b25fc7628b6de61e99c
ALEX-DKC <dinga8@uci.edu>
2025-10-20 18:45:27 -0700

    Update SoccerBall.cs

--- In Class Activities/Assets/W4/Scripts/SoccerBall.cs
+++ In Class Activities/Assets/W4/Scripts/SoccerBall.cs
@@ -23,19 +23,27 @@ public class SoccerBall : MonoBehaviour
 
     //private ?? ??
     //{
-        // finish STEP 1 by uncommenting and fixing the below line!
-        //Debug.Log(SoccerBall detected a collision with a trigger collider!);
+    // finish STEP 1 by uncommenting and fixing the below line!
+    //Debug.Log(SoccerBall detected a collision with a trigger collider!);
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("SoccerBall detected a collision with a trigger collider!");
 
-        // STEP 2 -------------------------------------------------------------
-        // Write an IF STATEMENT to check if the game object we collided with
-        //      has the tag "Goal".
-        // Refer to the slides for an example of how to check this :)
-        //
-        // Then, move your Debug.Log() statement so that it's only called if
-        //      the colliding object has the "Goal" tag.
+        if (other.CompareTag("Goal"))
+        {
+            _goalVFX.Play();
+        }
+    }
+    // STEP 2 -------------------------------------------------------------
+    // Write an IF STATEMENT to check if the game object we collided with
+    //      has the tag "Goal".
+    // Refer to the slides for an example of how to check this :)
+    //
+    // Then, move your Debug.Log() statement so that it's only called if
+    //      the colliding object has the "Goal" tag.
 
 
-        // STEP 2 -------------------------------------------------------------
+    // STEP 2 -------------------------------------------------------------
     //}
 
     // STEP 1 -----------------------------------------------------------------
@@ -49,17 +57,17 @@ public class SoccerBall : MonoBehaviour
     // Once you've created MadeGoal,
     //      move your Debug.Log() statement into MadeGoal, and
     //      call MadeGoal from inside your if statement in OnTriggerEnter.
-    
-        // STEP 4 -------------------------------------------------------------
-        // _goalVFX is a ParticleSystem, a Component for creating VFX.
-        // ParticleSystem has a method named Play() that displays the VFX:
-        // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
-        //
-        // Call Play on _goalVFX.
 
-        // STEP 4 -------------------------------------------------------------
-    
-    
+    // STEP 4 -------------------------------------------------------------
+    // _goalVFX is a ParticleSystem, a Component for creating VFX.
+    // ParticleSystem has a method named Play() that displays the VFX:
+    // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
+    //
+    // Call Play on _goalVFX.
+
+    // STEP 4 -------------------------------------------------------------
+
+
     // STEP 3 -----------------------------------------------------------------
 
 
@@ -78,7 +86,7 @@ public class SoccerBall : MonoBehaviour
     //      2. Use your MadeGoal method to update the points and text.
     //
     // STEP 5 -----------------------------------------------------------------
-    
+
 
     // STEP 6 -----------------------------------------------------------------
     // Like the last step, these flags do NOT show you where to put all of the
